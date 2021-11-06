36c6aca87092213298f4431ac6b5ab5f52972b14
blesscat <blesscat@gmail.com>
2021-11-06 23:01:54 +0800

    update

--- symlinks/.config/nvim/lua/lsp.lua
+++ symlinks/.config/nvim/lua/lsp.lua
@@ -11,7 +11,7 @@ local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  cmd [[hi LspDiagnosticsVirtualTextError guifg=#D0312D ctermfg=#D0312D gui=bold,italic,underline]]
+  cmd [[hi LspDiagnosticsVirtualTextError guifg=#D0312D ctermfg=red gui=bold,italic,underline]]
   -- cmd [[hi LspDiagnosticsVirtualTextWarning guifg=orange ctermfg=orange gui=bold,italic,underline]]
   cmd [[hi LspDiagnosticsVirtualTextInformation guifg=yellow ctermfg=yellow gui=bold,italic,underline]]
   cmd [[hi LspDiagnosticsVirtualTextHint guifg=green ctermfg=green gui=bold,italic,underline]]
@@ -56,6 +56,44 @@ for _, lsp in ipairs(servers) do
     }
   }
 end
+
+nvim_lsp.tailwindcss.setup {
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  filetypes = { "elm", "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "edge", "eelixir", "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte" },
+  init_options = {
+    userLanguages = {
+      eelixir = "html-eex",
+      eruby = "erb"
+    }
+  },
+  on_new_config = function(new_config)
+    if not new_config.settings then
+      new_config.settings = {}
+    end
+    if not new_config.settings.editor then
+      new_config.settings.editor = {}
+    end
+    if not new_config.settings.editor.tabSize then
+      -- set tab size for hover
+      new_config.settings.editor.tabSize = vim.lsp.util.get_effective_tabstop()
+    end
+  end,
+  root_dir = nvim_lsp.util.root_pattern('tailwind.config.cjs', 'tailwind.config.js', 'tailwind.config.ts','postcss.config.cjs', 'postcss.config.js', 'postcss.config.ts', 'package.json', 'node_modules', '.git'),
+  settings = {
+    tailwindCSS = {
+      lint = {
+        cssConflict = "warning",
+        invalidApply = "error",
+        invalidConfigPath = "error",
+        invalidScreen = "error",
+        invalidTailwindDirective = "error",
+        invalidVariant = "error",
+        recommendedVariantOrder = "warning"
+      },
+      validate = true
+    }
+  }
+}
 
 nvim_lsp.diagnosticls.setup {
   on_attach = on_attach,
